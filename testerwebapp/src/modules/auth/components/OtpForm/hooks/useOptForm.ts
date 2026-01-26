99c6484a9989e98b841edc8a27b47c9aeee6b344
unknown <soap4university@gmail.com>
2026-01-26 15:27:59 +0200

    mm

--- testerwebapp/src/modules/auth/components/OtpForm/hooks/useOptForm.ts
+++ testerwebapp/src/modules/auth/components/OtpForm/hooks/useOptForm.ts
@@ -1,7 +1,7 @@
 import { zodeResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { OtpScheme } from '../constants/OtpScheme';
-import type  OtpScheme  from '../constants/OtpScheme';
+import type OtpScheme from '../constants/OtpScheme';
 
 const useOptForm = () => {
   const otpForm = useForm<OtpScheme>({
